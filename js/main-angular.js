da1d91d657bc0ef7337a58c1ab830493b5422a53
Roman Zakharenkov <Roman Zakharenkov>
2016-12-11 01:40:52 +0200

    Added the pages planned, imporved the navigation bar as well as added an early version of a form for job posting

--- js/main-angular.js
+++ js/main-angular.js
@@ -1,13 +1,29 @@
-angular.module( 'repsikkaApp', [/* module includes */])
-.controller( 'repsikkaCtrl', [ '$scope', function( $scope )
+// To make a code clearer, all elements of the application to be called by the "app" variable.
+var app = angular.module( 'repsikkaApp', [/* module includes */]);
+
+app.controller( 'repsikkaCtrl', [ '$scope', function( $scope )
 {
    $scope.user =
    {
       'name' : 'John Doe',
       'balance' : 20.20
    };
-}])
-.directive( 'pageHeading', function()
+}]);
+
+app.controller('navbarCtrl', function($scope, $window){
+
+   // Function returns the link to the page if it leads not the current page, and returns link to void if the opposite is true.
+   $scope.getNavLink = function(link){
+      if( "/" + link == $window.location.pathname ){
+         return "javascript:void(do_something())"
+      }
+      else{
+         return link
+      }
+   };
+});
+
+app.directive( 'pageHeading', function()
 {
    console.log('header');
    return { //<-- This is a must here...
