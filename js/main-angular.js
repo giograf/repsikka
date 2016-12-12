0e1039c37ae4c88ef1580afe6199d45b3fe0d355
jersku <t5haje00@students.oamk.fi>
2016-12-12 04:30:13 +0200

    Made browse.html exepct the apply to job part. Added needed code at the end of main-angular.js.

--- js/main-angular.js
+++ js/main-angular.js
@@ -31,3 +31,70 @@ app.directive( 'pageHeading', function()
       templateUrl : 'page-head.html'
    };
 });
+
+app.controller("filterCtrl", function ($scope) {
+
+  $scope.jobs = [{
+    "name": "Honey Delivering job",
+    "location": "Oulu",
+    "category": "Deliver",
+    "date": "21/12/2016",
+    "pay": "6€",
+    "descreption": "According to all known laws of aviation there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+  }, {
+    "name": "Looking for cook",
+    "location": "Oulu",
+    "category": "Cook",
+    "date": "15/12/2016",
+    "pay": "10€",
+    "descreption": "You make food me naow."
+  },
+  {
+   "name": "Moving job",
+   "location": "Oulu",
+   "category": "Deliver",
+   "date": "15/12/2016",
+   "pay": "10€",
+   "descreption": "Help I need a person with car to move my stuff."
+ },
+ {
+  "name": "Dirty room",
+  "location": "Oulu",
+  "category": "Cleaning",
+  "date": "13/12/2016",
+  "pay": "15€",
+  "descreption": "My mom says my room is too dirty help clean."
+},
+{
+ "name": "Looking for tutor",
+ "location": "Oulu",
+ "category": "Tutoring",
+ "date": "16/12/2016",
+ "pay": "40€",
+ "descreption": "Can't seem to learn swedish need help"
+},
+    {
+    "name": "Need a baby sittah",
+    "location": "Oulu",
+    "category": "Babysitting",
+    "date": "13/12/2016",
+    "pay": "30€",
+    "descreption": "My babyboy requires sitting."
+}];
+
+  $scope.FilteringArray = {};
+  $scope.index = 0;
+  $scope.categoryFilter = function(job) {
+    return $scope.FilteringArray[job.category] || Allcategories($scope.FilteringArray);
+  };
+
+  function Allcategories(filterObj) {
+  for (var key in filterObj) {
+  if (filterObj[key]) {
+    console.log($scope.FilteringArray);
+    return false;
+  }
+}
+    return true;
+  }
+});
