64a6afc946920672b83c4ee83d7cf999ccab7a1b
Aatu Mikkonen <t5miaa00@students.oamk.fi>
2016-12-13 18:20:59 +0200

    changed the identation a little bit

--- js/main-angular.js
+++ js/main-angular.js
@@ -5,18 +5,18 @@
 // var somevar = getUrlParameter('parameter');
 
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+   for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
+      if (sParameterName[0] === sParam) {
+         return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+   }
 };
 
 var job_list = [{
